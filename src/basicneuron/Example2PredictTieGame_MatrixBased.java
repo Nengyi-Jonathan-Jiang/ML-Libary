14495088055bb0bdac766b2e977ab91843c0607f
K1334755 <theNerdyMango@gmail.com>
2023-11-13 09:06:11 -0600

    sad math

--- src/basicneuron/Example2PredictTieGame_MatrixBased.java
+++ src/basicneuron/Example2PredictTieGame_MatrixBased.java
@@ -8,8 +8,8 @@ import neuralnet.neuron.ActivationFunction;
 import neuralnet.neuron.LossFunction;
 
 public class Example2PredictTieGame_MatrixBased {
-    private static final double LEARNING_RATE = 0.1;
-    private static final int BATCH_SIZE = 100;
+    private static final double LEARNING_RATE = 0.05;
+    private static final int BATCH_SIZE = 1000;
 
     private static double[] __generate_test_case() {
         double WR = Math.random() * 0.6 + 0.2;
@@ -17,21 +17,23 @@ public class Example2PredictTieGame_MatrixBased {
         double WR_eva = 0.7;
         double AG_eva = 3.5;
 
-        double __skill_difference = AG - AG_eva + 5 * (WR - WR_eva);// + Math.random() - 0.5;
-        double didHaveTie = Math.abs(__skill_difference) <= 0.2 ? 1 : 0;
+        double __skill_difference = AG - AG_eva + 2 * (WR - WR_eva);// + Math.random() - 0.5;
+        double didHaveTie = Math.abs(__skill_difference) <= 0.1 ? 1 : 0;
         double didTeamScoreHigh = Math.max(AG * (WR_eva + 0.5) / 2, AG_eva * (WR + 0.5) / 2) >= 4 ? 1 : 0;
 
-        return new double[]{WR, AG, didHaveTie, didTeamScoreHigh};
+//        return new double[]{WR, AG, didHaveTie, didTeamScoreHigh};
+        return new double[]{WR, AG, Math.abs(WR - 0.5) > 0.15 ? 1 : 0, 0};
     }
 
     public static void main(String[] args) {
         Model model = new Model(LossFunction.LogLoss, LEARNING_RATE);
         model.addLayer(new DenseLayer(3, 3, ActivationFunction.ReLU));
         model.addLayer(new DenseLayer(3, 2, ActivationFunction.Sigmoid));
+//        model.addLayer(new DenseLayer(3, 2, ActivationFunction.Sigmoid));
 
         LineChart chart = new LineChart();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
             Model.DataPoint[] dataPoints = new Model.DataPoint[BATCH_SIZE];
             for(int j = 0; j < BATCH_SIZE; j++) {
                 double[] testCase = __generate_test_case();
